45257cf931631c9f7e33c26cca0e893552a5f08e
shantaisat <shanta.mahoneyis@gmail.com>
2025-03-05 00:31:25 +0100

    readies code for deploy

---
+++ blog/views.py
@@ -0,0 +1,6 @@
+from django.http import HttpResponse
+
+def my_blog(request):
+    return HttpResponse("Hello, blog!")
+
+# Create your views here.
---
+++ env.py
@@ -0,0 +1,2 @@
+# filepath: /Users/inkwell/Documents/vscode_projects/codestar_blog/env.py
+
