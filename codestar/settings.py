a5c8e01d0baa7df5465a0926b4878bf4e9a14099
shantaisat <shanta.mahoneyis@gmail.com>
2025-03-05 02:57:48 +0100

    connected to PostgreSQL and created superuser

--- codestar/settings.py
+++ codestar/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 import dj_database_url
-from pathlib import Path
+from pathlib import Path 
 
 if os.path.isfile("env.py"):
     import env
@@ -79,13 +79,15 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
--- env.py
+++ env.py
@@ -1,2 +1,6 @@
 # filepath: /Users/inkwell/Documents/vscode_projects/codestar_blog/env.py
+import os
+
+os.environ.setdefault(
+    "DATABASE_URL", "postgresql://neondb_owner:npg_1MtfI8iuxejp@ep-crimson-morning-a2gb5wyk.eu-central-1.aws.neon.tech/brim_fresh_press_867391")
 
