ee5241f67e5b049fc18157f96c8bf45506c32ea9
shantaisat <shanta.mahoneyis@gmail.com>
2025-03-05 10:12:32 +0100

    Added SECRET_KEY from environment variables

--- env.py
+++ env.py
@@ -4,3 +4,4 @@ import os
 os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:npg_1MtfI8iuxejp@ep-crimson-morning-a2gb5wyk.eu-central-1.aws.neon.tech/brim_fresh_press_867391")
 
+os.environ.setdefault("SECRET_KEY", "Pine@pple8347Xz$")
