79e1fed953ae330567a36bbc1cd91d387aa565c1
shantaisat <shanta.mahoneyis@gmail.com>
2025-03-07 13:02:53 +0100

    added comment to models.py

--- blog/models.py
+++ blog/models.py
@@ -21,7 +21,7 @@ class Comment(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
     body = models.TextField()
     approved = models.BooleanField(default=False)
-   created_on = models.DateTimeField(auto_now_add=True)
+    created_on = models.DateTimeField(auto_now_add=True)
     
     
 
