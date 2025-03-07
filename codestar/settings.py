3ae7fa208328d9ba511b90ef61148f8188a1633d
shantaisat <shanta.mahoneyis@gmail.com>
2025-03-07 10:20:30 +0100

    added admin comment

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'Pine@pple8347Xz$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1',
                  'codestar-is-blog-b6bff87418f6.herokuapp.com',
